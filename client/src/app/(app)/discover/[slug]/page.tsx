1a6cfa396d27466fd31749c2b962799d2e8c8f8a
DFirenice <firenic.firenice@gmail.com>
2025-05-07 22:16:59 -0400

    Discover feed (v0.21) UI updates

--- client/src/app/(app)/discover/[slug]/page.tsx
+++ client/src/app/(app)/discover/[slug]/page.tsx
@@ -13,105 +13,68 @@ const DiscoveryThemePage = () => {
     return (
         <div
             className="
-                overflow-y-scroll hide-scrollbar w-full snap-y snap-mandatory
-                relative bg-cover bg-center
+                overflow-y-scroll w-full
+                snap-y snap-mandatory snap-always scroll-p-[10%]
+                *:first-of-type:mt-[10dvh]
             "
-            style={{ backgroundImage: `url(${imgUrl})` }}
+            style={{ background: `
+                linear-gradient(0deg, rgba(0, 0, 0, .72), rgba(0, 0, 0, .72)),
+                center / cover no-repeat url(${imgUrl})
+            ` }}            
         >
-            <section className="relative h-min-[70dvh] h-full w-full snap-start">
-                <div className="
-                    absolute inset-0 backdrop-blur-lg bg-black/70
-                    flex justify-center items-center gap-[3rem]
-                ">
-                    <div className="relative w-[22dvw] aspect-[1/1.5]">
-                        <Image src={imgUrl} fill className="object-cover rounded-2xl" alt="Discover" />
-                    </div>
-                    <div className="flex flex-row gap-4">
-                        <div className="w-[10dvw] aspect-square bg-pink-200 rounded-xl">
+            {[...Array(5)].map((_, i) => (
+                <section key={i} className="relative h-[80%] w-full snap-start overflow-hidden">
+                    <div className="
+                        absolute inset-0
+                        flex justify-center items-center gap-[3rem]
+                    ">
+                        <div className="relative w-[16dvw] aspect-[1/2]">
+                            <Image src={imgUrl} fill className="object-cover rounded-2xl" alt="Discover" />
+                        </div>
+                        <div className="flex flex-row gap-4">
                             {/* Belonging album image */}
-                        </div>
-                        <div className="space-y-4">
-                            <Heading level={2} size="large">Linkin Park Mix</Heading>
-                            {/* Playlist data */}
-                            <div className="
-                                flex items-center text-sm text-muted-foreground
-                                [&>*:not(:first-child)]:before:content-['•'] [&>*:not(:first-child)]:before:mx-2
-                            ">
-                                <span>By <a className="text-accent-default underline">Spotify</a></span>
-                                <span>Playlist year</span>
-                                <span>22 songs</span>
-                                <span>Total duration</span>
-                            </div>
-                            {/* Controls */}
-                            <div className="space-x-1.5">
-                                <Button variant="none" size="icon">
-                                    <Icon id="play" />
-                                </Button>
-                                <Button variant="ghost" className="p-0 gap-0">
-                                    <Icon id="save" />
-                                    <span className="pr-2 text-fg-secondary">622k</span>
-                                </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="add_to_playlist" /> </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="add_to_queue" /> </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="share" /> </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="more" /> </Button>
-                            </div>
-                            {/* Tags */}
-                            <div className="space-x-2">
-                                <Tag text="Rock" secondary />
-                                <Tag text="Rock Alt" secondary />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="relative h-min-[70dvh] h-full w-full snap-start">
-                <div className="
-                    absolute inset-0 backdrop-blur-lg bg-black/70
-                    flex justify-center items-center gap-[3rem]
-                ">
-                    <div className="relative w-[22dvw] aspect-[1/1.5]">
-                        <Image src={imgUrl} fill className="object-cover rounded-2xl" alt="Discover" />
-                    </div>
-                    <div className="flex flex-row gap-4">
-                        <div className="w-[10dvw] aspect-square bg-pink-200 rounded-xl">
-                            {/* Belonging album image */}
-                        </div>
-                        <div className="space-y-4">
-                            <Heading level={2} size="large">Linkin Park Mix</Heading>
-                            {/* Playlist data */}
-                            <div className="
-                                flex items-center text-sm text-muted-foreground
-                                [&>*:not(:first-child)]:before:content-['•'] [&>*:not(:first-child)]:before:mx-2
-                            ">
-                                <span>By <a className="text-accent-default underline">Spotify</a></span>
-                                <span>Playlist year</span>
-                                <span>22 songs</span>
-                                <span>Total duration</span>
-                            </div>
-                            {/* Controls */}
-                            <div className="space-x-1.5">
-                                <Button variant="none" size="icon">
-                                    <Icon id="play" />
-                                </Button>
-                                <Button variant="ghost" className="p-0 gap-0">
-                                    <Icon id="save" />
-                                    <span className="pr-2 text-fg-secondary">622k</span>
-                                </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="add_to_playlist" /> </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="add_to_queue" /> </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="share" /> </Button>
-                                <Button variant="ghost" size="icon"> <Icon id="more" /> </Button>
-                            </div>
-                            {/* Tags */}
-                            <div className="space-x-2">
-                                <Tag text="Rock" secondary />
-                                <Tag text="Rock Alt" secondary />
+                            <div className="w-[10dvw] aspect-square bg-pink-200 rounded-xl"></div>
+
+                            <div className="space-y-4">
+                                <Heading level={2} size="large">Linkin Park Mix</Heading>
+
+                                {/* Playlist data */}
+                                <div className="
+                                    flex items-center text-sm text-muted-foreground
+                                    [&>*:not(:first-child)]:before:content-['•'] [&>*:not(:first-child)]:before:mx-2
+                                ">
+                                    <span>By <a className="text-accent-default underline">Spotify</a></span>
+                                    <span>Playlist year</span>
+                                    <span>22 songs</span>
+                                    <span>Total duration</span>
+                                </div>
+
+                                {/* Controls */}
+                                <div className="inline-flex items-center gap-x-2">
+                                    <Button variant="none" size="icon">
+                                        <Icon size="large" id="play" />
+                                    </Button>
+                                    <Button variant="ghost" className="p-0 gap-0">
+                                        <Icon id="save" />
+                                        <span className="pr-2 text-fg-secondary">622k</span>
+                                    </Button>
+                                    <Button variant="ghost" size="icon"> <Icon id="add_to_playlist" /> </Button>
+                                    <Button variant="ghost" size="icon"> <Icon id="add_to_queue" /> </Button>
+                                    <Button variant="ghost" size="icon"> <Icon id="share" /> </Button>
+                                    <Button variant="ghost" size="icon"> <Icon id="more" /> </Button>
+                                </div>
+
+                                {/* Tags */}
+                                <div className="space-x-2">
+                                    <Tag text="Rock" secondary />
+                                    <Tag text="Rock Alt" secondary />
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            ))}
         </div>
     )
 }
